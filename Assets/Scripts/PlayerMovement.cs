8f9909dd64df70b58c5a11ebdc871c99fa0d2efa
joryu <34364890+joryu@users.noreply.github.com>
2021-03-20 17:48:05 -0700

    - new player mvt and ground pushed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,22 +8,24 @@ public class PlayerMovement : MonoBehaviour
     private float moveSpeed = 5f;
 
     [SerializeField]
-    private Rigidbody2D rb;
+    private Rigidbody rb;
 
     private Vector2 movement;
 
     void Update()
     {
         // Process input.
-        movement.x = Input.GetAxisRaw("Horizontal"); // gives -1 to 1
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement = movement.normalized;
+        movement.x = Input.GetAxis("Horizontal"); // gives -1 to 1
+        movement.y = Input.GetAxis("Vertical");
+        movement.Normalize();
+
+        rb.velocity = new Vector3(movement.x * moveSpeed, rb.velocity.y, movement.y * moveSpeed);
     }
 
-    void FixedUpdate()
-    {
+   // void FixedUpdate()
+    //{
         // Actual movement stuff.
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+       // rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         // transform.Translate(new Vector3(movement.x, movement.y, 0) * moveSpeed * Time.fixedDeltaTime);
-    }
+   // }
 }
