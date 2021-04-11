586e13f88019ca12fb4210e8593d90b81a8b1c29
chungsi <ashleyctsang@gmail.com>
2021-04-10 18:52:02 -0700

    Some adjustments to scene loading/changing

--- Assets/Scripts/Interactables/Interactable_SceneLoadOverlay.cs
+++ Assets/Scripts/Interactables/Interactable_SceneLoadOverlay.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Add this file to a gameObject that is named the same as the scene it
+/// should load as an overlay.
+/// </summary>
 public class Interactable_SceneLoadOverlay : MonoBehaviour, IInteractable
 {
     bool isSceneLoaded = false;
+
     public void Interact(GameObject _other)
     {
-        Debug.Log("Aight imma head out for real");
+        Debug.Log("Aight imma do some work");
 
         if (!isSceneLoaded)
         {
-            SceneManager.LoadScene("Crafting", LoadSceneMode.Additive);
+            SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+            isSceneLoaded = true;
         }
         else
         {
-            SceneManager.UnloadSceneAsync("Crafting");
+            SceneManager.UnloadSceneAsync(gameObject.name);
+            isSceneLoaded = false;
         }
-        isSceneLoaded = !isSceneLoaded;
     }
 }
